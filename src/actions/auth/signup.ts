9087d9225cce7a2a9f1da679bdf533c5a56d8d8d
rumpapl <rumpapaul0201@gmail.com>
2025-05-24 20:29:18 +0600

    feat(signup-action, prettier): write server action for signup user, configure and activate prettier

---
+++ src/actions/auth/signup.ts
@@ -0,0 +1,33 @@
+"use server";
+
+import getDatabase from "@/lib/mongodb";
+import bcrypt from "bcryptjs";
+
+import { signupUserType } from "@/types";
+
+const COLLECTION_NAME: string = "user";
+
+export const signupUserAction = async ({
+  name,
+  email,
+  password,
+}: signupUserType) => {
+  const db = await getDatabase();
+
+  const existingUser = await db.collection("users").findOne({ email: email });
+  if (existingUser) {
+    throw new Error("User already exist");
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 12);
+
+  await db
+    .collection(COLLECTION_NAME)
+    .insertOne({
+      name,
+      email,
+      password: hashedPassword,
+      createdAt: new Date(),
+    });
+  return { message: "User created successfully." };
+};
---
+++ src/types/auth.ts
@@ -0,0 +1,5 @@
+export type signupUserType = {
+  name: string;
+  email: string;
+  password: string;
+};
